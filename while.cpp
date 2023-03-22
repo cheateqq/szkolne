6f5191d558ee1ac39def0a9b39f0ed3379292dc0
cheateqq <125015608+cheateqq@users.noreply.github.com>
2023-03-22 11:05:40 +0100

    Create while.cpp

---
+++ while.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int wplata,suma_wplat=0,zbieranie;
+    
+    cout<<"Ile zbieramy: ";
+    cin>>zbieranie;
+    
+    while(suma_wplat<zbieranie){
+        cout<<endl<<"Wprowadz kwote wplaty: ";
+        cin>>wplata;
+        suma_wplat=suma_wplat+wplata;
+    }
+    
+    cout<<endl<<"Uzbierana kwota: "<<suma_wplat;
+}
